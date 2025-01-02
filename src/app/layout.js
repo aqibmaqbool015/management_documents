a753a56c87d564d5fe5f9641dae94f8613282de1
aqibmaqbool015 <engnraqib1122@gmail.com>
2025-01-02 14:34:46 +0500

    fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,7 +39,6 @@ export default function RootLayout({ children }) {
             {children}
           </div>
         </div>
-        {/* {!isAuthPage && <Footer />} */}
       </body>
     </html>
   );
